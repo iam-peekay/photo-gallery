56ab80bf9d4531699513cedd72cdd86ee65b4dc9
iam-peekay <iam.preethi.k@gmail.com>
2016-03-29 21:30:25 -0700

    Update readme with instructions on how to run

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # photo-gallery
+
+### To run the application:
+```
+git clone https://github.com/iam-peekay/photo-gallery.git
+cd photo-gallery
+npm install
+npm start
+```
+Open your browser and visit http://localhost:3000
--- server.prod.js
+++ server.prod.js
@@ -1,19 +1,19 @@
-// var path = require('path');
-// var express = require('express');
-// var app = express();
-// var port = process.env.PORT || 3000;
-//
-// app.use(express.static(path.join(__dirname, 'src')));
-//
-// // default catch all
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
-//
-// app.listen(port, (error) => {
-//   if (error) {
-//     console.error(error)
-//   } else {
-//     console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-//   }
-// });
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+// default catch all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+});
